a77a490148fab6b59830952b768edb3f12d13791
Zesheng Xing <zsxing99@gmail.com>
2019-11-13 20:11:18 -0800

    folder reorder

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TopNavBar } from "./TopNavBar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar />
+    </div>
+  );
+}
+
+export default App;
